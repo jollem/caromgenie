e971b993dc6c79debc903d9e38affea6e738557f
Lauri Arimo <lauri.arimo@wunderdog.fi>
2023-09-17 00:30:01 +0300

    Tweak navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,18 +1,48 @@
 import { useContext } from "react";
+import classnames from "classnames";
 import { GameContext } from "../../store/GameContext";
 import styles from "./NavBar.module.css";
 
+const MenuBurger = () => (
+  <div
+    className={`${styles.menu} ${styles.burger}`}
+    onClick={() => console.log("rai")}
+  >
+    <span />
+    <span />
+    <span />
+  </div>
+);
+
+const PauseToggle = ({
+  toggle,
+  running,
+}: {
+  toggle?: () => void;
+  running: boolean;
+}) => (
+  <div onClick={toggle} className={`${styles.menu} ${styles.toggle}`}>
+    <span className={classnames(running ? styles.pause : styles.play)} />
+  </div>
+);
+/*<p>
+Innings:{" "}
+{Math.max(...gameState.players.map((player) => player.innings.length))}
+</p>
+<p>Started: {gameState.timestamp}</p>
+<p>Duration: {gameState.duration}</p> */
 const NavBar = () => {
   const gameState = useContext(GameContext);
   return (
-    <div className={styles.navbar}>
-      <p>
-        Innings:{" "}
-        {Math.max(...gameState.players.map((player) => player.innings.length))}
-      </p>
-      <p>Started: {gameState.timestamp}</p>
-      <p>Duration: {gameState.duration}</p>
-    </div>
+    <nav className={styles.navbar}>
+      <MenuBurger />
+      {gameState.active >= 0 && (
+        <PauseToggle
+          toggle={gameState.pauseToggle}
+          running={gameState.running}
+        />
+      )}
+    </nav>
   );
 };
 
