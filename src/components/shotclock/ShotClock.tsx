4d9fb614eb6f801316b353e6e12a4fdf7470fefa
Lauri Arimo <lauri.arimo@wunderdog.fi>
2023-09-17 01:06:12 +0300

    Tweak shotclock

--- src/components/shotclock/ShotClock.tsx
+++ src/components/shotclock/ShotClock.tsx
@@ -1,11 +1,25 @@
 import { useContext } from "react";
+import classnames from "classnames";
 import { GameContext } from "../../store/GameContext";
 import styles from "./ShotClock.module.css";
 
 const ShotClock = () => {
   const gameState = useContext(GameContext);
   return (
-    <div className={styles.shotclock}>ShotClock: {gameState.shotclock}</div>
+    <div className={styles.shotclock}>
+      {gameState.active >= 0 &&
+        Array.from({ length: 40 }, (value, _index) => value).map((_, index) => {
+          return (
+            <span
+              key={index}
+              className={classnames({
+                [styles.warn]: index < 10,
+                [styles.hide]: index + gameState.shotclock >= 40,
+              })}
+            />
+          );
+        })}
+    </div>
   );
 };
 
