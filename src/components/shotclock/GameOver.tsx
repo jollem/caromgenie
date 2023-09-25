1b27241b212f34f7514e3e90fc5977e85bba3bf0
Lauri Arimo <lauri.arimo@wunderdog.fi>
2023-09-25 10:59:44 +0300

    Add some confetti to GameOver screen

--- src/components/shotclock/GameOver.tsx
+++ src/components/shotclock/GameOver.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { FaHandshake, FaCrown } from "react-icons/fa";
 import classnames from "classnames";
+import ConfettiExplosion from "react-confetti-explosion";
 import { GameContext } from "../../store/GameContext";
 import styles from "./ShotClock.module.scss";
 
@@ -21,6 +22,11 @@ const GameOver = () => {
     <div className={classnames(styles.centerSelf, styles.gameOver)}>
       {scores.indexOf(max) === scores.lastIndexOf(max) ? (
         <>
+          <ConfettiExplosion
+            force={1.5}
+            width={window.innerWidth * 1.2}
+            duration={3000}
+          />
           <FaCrown /> {balls[scores.indexOf(max)]} <FaCrown />
         </>
       ) : (
