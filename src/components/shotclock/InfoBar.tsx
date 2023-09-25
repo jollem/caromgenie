92ba7f45126214ae30bdbe5227bd471855866438
Lauri Arimo <lauri.arimo@wunderdog.fi>
2023-09-25 11:11:36 +0300

    Finetune game start up screen (show ⚪️ insted of player icon)

--- src/components/shotclock/InfoBar.tsx
+++ src/components/shotclock/InfoBar.tsx
@@ -1,14 +1,15 @@
 import { useContext } from "react";
-import { FaHandPointRight, FaUser } from "react-icons/fa";
+import { FaHandPointRight } from "react-icons/fa";
+import classnames from "classnames";
 import { GameContext } from "../../store/GameContext";
 import styles from "./ShotClock.module.scss";
 
 const InfoBar = () => {
   const gameState = useContext(GameContext);
   return gameState.started ? null : (
-    <div className={styles.centerSelf}>
+    <div className={classnames(styles.centerSelf, styles.gameOver)}>
       <FaHandPointRight />
-      <FaUser />
+      {"⚪️"}
     </div>
   );
 };
